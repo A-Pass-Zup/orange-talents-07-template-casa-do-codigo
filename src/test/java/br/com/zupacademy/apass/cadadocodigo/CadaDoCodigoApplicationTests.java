8f33727002a01763e21ca26224444f752a2c32ef
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-27 10:04:56 -0300

    Inicializa o projeto

---
+++ src/test/java/br/com/zupacademy/apass/cadadocodigo/CadaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.apass.cadadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
